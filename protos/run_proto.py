ace0399b5ce8106438864265aa625850590a53ba
Iulian Stana <julian.stana@gmail.com>
2017-04-20 21:59:44 +0300

    Move proto file in a top level folder where it can be access

--- protos/run_proto.py
+++ protos/run_proto.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     if target_dir != target_script:
         os.chdir(target_dir)
 
-    subcommand = 'python -m grpc.tools.protoc -I . --python_out . --grpc_python_out . *.proto'
+    subcommand = 'python -m grpc.tools.protoc -I . --python_out . --grpc_python_out . *.protos'
     subprocess.call(subcommand, shell=True)
